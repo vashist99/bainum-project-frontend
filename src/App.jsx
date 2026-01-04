ec50418fe1831a6cd30d14db9304ce5ef489352a
Vashist Hegde <vashistgh99@gmail.com>
2026-01-04 10:29:30 -0800

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,6 @@ import TeacherRegisterPage from './pages/TeacherRegisterPage';
 import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
-  // #region agent log
-  fetch('http://127.0.0.1:7242/ingest/8ed3ab10-f37b-4a7f-b1ef-b646b3b66295',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({location:'App.jsx:14',message:'App component mounted',data:{pathname:window.location.pathname,hash:window.location.hash,search:window.location.search},timestamp:Date.now(),sessionId:'debug-session',runId:'run1',hypothesisId:'A'})}).catch(()=>{});
-  // #endregion
-  
   return <div data-theme="forest" className="min-h-screen">
     <Routes>
       <Route path="/" element={<LoginPage />} />
